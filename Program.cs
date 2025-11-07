65859225c5297dd075c58978bdce69c5f7c6e34e
Estudiante <Estudiante@ULELAB505>
2025-11-06 22:10:37 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Ingrese la medida de la base del rectangulo: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la medida de la altura del rectangulo: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            //llamar a la funcion
+            double resultado = num1 * num2;
+            Console.WriteLine($"{num1} * {num2} = {resultado}");
+            
+
+
+            Console.WriteLine($"El area del rectangulo es: {resultado}");
+
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
